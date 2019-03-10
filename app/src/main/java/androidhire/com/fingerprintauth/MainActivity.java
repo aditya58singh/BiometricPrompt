f9755ef8720451126d5172400b5751dbf6b69c0e
aditya <aditya58singh@gmail.com>
2019-03-10 11:01:39 +0530

    1 commit

---
+++ app/src/main/java/androidhire/com/fingerprintauth/MainActivity.java
@@ -0,0 +1,54 @@
+package androidhire.com.fingerprintauth;
+
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.fragment.app.FragmentActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Executor newExecutor = Executors.newSingleThreadExecutor();
+
+        FragmentActivity activity = this;
+
+        final BiometricPrompt myBiometricPrompt = new BiometricPrompt(activity, newExecutor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON) {
+                } else {
+                    // Log.d(TAG, "Error occurred");
+                }
+            }
+
+        });
+
+        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Touch to Unlock")
+                .setNegativeButtonText("Cancel")
+                .build();
+
+        findViewById(R.id.launchAuthentication).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myBiometricPrompt.authenticate(promptInfo);
+            }
+        });
+
+    }
+}
